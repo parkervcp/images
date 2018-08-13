170aba2c5997002e3cbd67465ab66c9e3efed650
Friesi <friesi@srvb.at>
2018-08-13 20:05:59 +0200

    install dependencies for the bot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
-#!/bin/ash
+#!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version
+# Output Current Python Version
+python --version
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Install Python requirements
+if [ -d Red-DiscordBot ]; then
+  cd /home/container/Red-DiscordBot
+  python3 launcher.py --update-red
+fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
