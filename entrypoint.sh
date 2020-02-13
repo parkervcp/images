bd41b46738cb15c38ddbcaf83416ee5e7d5e10a0
Dmitri233 <36127740+Dmitri233@users.noreply.github.com>
2020-02-14 00:47:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,10 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
-echo -e ":/home/container$ ${START_COMMAND}"
+# echo
+echo -e ":/home/container$ ${startvariable}"
 
-#Test
-export LD_PRELOAD=./preload.so
-export LD_LIBRARY_PATH=.
 
 # Run the Server
 LD_LIBRARY_PATH=.
-LD_PRELOAD=./preload.so ./bedrock_server
-${MODIFIED_STARTUP}
+LD_PRELOAD=${startvariable} ./bedrock_server
