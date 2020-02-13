1ead28359d3db88600b59c8a9b78aaeccb95a42d
Dmitri233 <36127740+Dmitri233@users.noreply.github.com>
2020-02-13 21:19:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,11 @@ MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
 echo -e ":/home/container$ ${START_COMMAND}"
 
+#Test
+export LD_PRELOAD=./preload.so
+export LD_LIBRARY_PATH=.
+
 # Run the Server
+LD_LIBRARY_PATH=.
+LD_PRELOAD=./preload.so ./bedrock_server
 ${MODIFIED_STARTUP}
